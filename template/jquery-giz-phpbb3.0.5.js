13cc85318e1ae6f0917d133b78818c8910eecd7e
Jeremy <access@crowna.com>
2015-09-17 21:26:38 +1200

    style finished

--- template/jquery-giz-phpbb3.0.5.js
+++ template/jquery-giz-phpbb3.0.5.js
@@ -10,6 +10,24 @@ $( function () {
 		jq_view_drop ( '.giz-uadmin' )
 	  }
 	);
+
+    //forum lists
+    $('div.forabg ul.forums dd').css('float','left' );
+
+    var cnt = 0;
+    var li_list = {};
+    var tmp = '';
+    $('div.forabg ul.forums dd').each(function(){
+        tmp = $(this).parent().parent() ;
+        if (  !tmp.hasClass('done') ){
+           // typeof li_list['li'+cnt] == "undefined" &&
+            tmp.addClass('done');
+            li_list['li'+cnt] = tmp.css('height');
+            cnt ++;
+        }
+       // $(this).css('height', li_list['li'+(cnt-1)] );
+        $(this).css('height', tmp.css('height') );
+    });
 } );
 
 // Creating a new function, fadeToggle()
